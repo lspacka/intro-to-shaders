a75b0d0274ac3b93e2f9ac666424b0863c976dc8
julian <jdrc@protonmail.com>
2024-02-10 16:34:28 -0500

    lesson 2 complete

--- src/sketches/001-getting-started.glsl
+++ src/sketches/001-getting-started.glsl
@@ -5,7 +5,14 @@ uniform float uTime;
 
 // The most basic shader
 void main(){
-  vec3 color = vec3(1.);
+  // vec2 uv = vUv;
+  vec3 color = vec3(1., 0., 0.);
+  if (vUv.x > 0.5) {
+    color = vec3(0., 0., 1.);
+  }
+  if (vUv.y < 0.5) {
+    color = vec3(0., 1., 0.);
+  }
 
-  gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, 0.1);
 }
